fcb55db0c658ab1f7eab719ef509d05f7c855018
oldcastle <32189785+oIdcastle@users.noreply.github.com>
2021-04-29 12:28:39 +0200

    Implement checkOverpopulation() method

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,10 @@
 package hu.nive.ujratervezes.jurassic;
 
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -11,5 +16,22 @@ public class JurassicPark {
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
+
+    List<String> checkOverpopulation() {
+        List<String> dinoNames = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(this.dbUrl, this.dbUser, this.dbPassword)) {
+            String sql = "SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed";
+            PreparedStatement pstmt = connection.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery();
+
+            while(rs.next()) {
+                dinoNames.add(rs.getString(1));
+            }
+        } catch (SQLException e) {
+            System.out.println("Database error: " + e.getMessage());
+        }
+        return dinoNames;
+    }
+
 
 }
